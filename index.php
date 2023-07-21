24b10255f102108dfcf7ce150bed4a2a733e911f
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-21 16:47:36 +0200

    removed setAge method

--- index.php
+++ index.php
@@ -50,35 +50,31 @@ class Actor
 }
 
 // attori imitation game
-$actor1 = new Actor('Benedict', 'Cumberbatch', 38);
+$actor1 = new Actor('Benedict', 'Cumberbatch', 33);
+$actor1->setAge(34);
 $actor2 = new Actor('Jimm', 'Logan', 40);
 // attori interstellar
 $actor3 = new Actor('Matthew', 'McConaughey', 50);
 $actor4 = new Actor('Maria', 'Ginni', 27);
 
 $imitation_game_actors = [
-    $main_actor = $actor1->getFullName(),
-    $secondary_actor = $actor2->getFullName()
+    $main_actor = $actor1->getFullName() . ' che ha ' . $actor1->age . ' anni',
+    $secondary_actor = $actor2->getFullName() . ' che ha ' . $actor2->age . ' anni'
 ];
 
 $interstellar_actors = [
-    $main_actor = $actor3->getFullName(),
-    $secondary_actor = $actor4->getFullName()
+    $main_actor = $actor3->getFullName() . ' che ha ' . $actor3->age . ' anni',
+    $secondary_actor = $actor4->getFullName() . ' che ha ' . $actor4->age . ' anni'
 ];
 
-// var_dump($imitation_game_actors);
-// var_dump($interstellar_actors);
+var_dump($imitation_game_actors);
+var_dump($interstellar_actors);
 
 
 $films = [
     $imitation_game = new Movie('The Imitation Game', 2014, 'Drammatico', $imitation_game_actors),
     $interstellar = new Movie('Interstellar', 2016, 'Fantascienza', $interstellar_actors)
 ];
-
-// var_dump($films[0]);
-
-// var_dump($films[1]);
-
 
 ?>
 
